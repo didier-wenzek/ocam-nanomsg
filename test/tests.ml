008c14e756f55b95b89af58ac142e6586fd7d1ab
Didier Wenzek <didier.wenzek@acidalie.com>
2017-12-11 17:19:33 +0100

    Add smoke test.

---
+++ test/tests.ml
@@ -0,0 +1,32 @@
+open Lwt
+
+let send_msgs socket =
+  Lwt_list.iter_s (Nanomsg.send socket)
+
+let expect_msgs socket =
+  let expect msg =
+    Nanomsg.recv socket
+    >>= fun actual_msg ->
+    Printf.printf "XOXOX received = %s\n%!" actual_msg;
+    assert (actual_msg = msg);
+    return ()
+  in
+  Lwt_list.iter_s expect
+    
+let () =
+  let producer = Nanomsg.socket Nanomsg.Push in
+  let consumer = Nanomsg.socket Nanomsg.Pull in
+
+  let _ = Nanomsg.bind producer "inproc://foo" in
+  let _ = Nanomsg.connect consumer "inproc://foo" in
+(*
+  let _ = Nanomsg.bind producer "ipc:///tmp/foo" in
+  let _ = Nanomsg.connect consumer "ipc:///tmp/foo" in
+*)
+
+  let msg_list = ["foo"; "bar"; "xoxox"] in
+
+  Lwt.join [
+    send_msgs producer msg_list;
+    expect_msgs consumer msg_list;
+  ] |> Lwt_main.run
