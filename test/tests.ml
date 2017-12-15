dd8c5d19b5b2a8d6df461ef672b36b17a1af7f50
Didier Wenzek <didier.wenzek@acidalie.com>
2017-12-15 11:56:12 +0100

    Test when things go wrong.

--- test/tests.ml
+++ test/tests.ml
@@ -7,6 +7,14 @@ let test_lwt name f =
   in
   name >:: test
 
+let test_lwt_fail name exn f =
+  let test test_ctxt =
+    assert_raises exn (fun () ->
+      Lwt_main.run (f ())
+    )
+  in
+  name >:: test
+
 let send_msgs socket =
   Lwt_list.iter_s (Nanomsg.send socket)
 
@@ -14,7 +22,6 @@ let expect_msgs socket =
   let expect msg =
     Nanomsg.recv socket
     >>= fun actual_msg ->
-    Printf.printf "XOXOX received = %s\n%!" actual_msg;
     assert (actual_msg = msg);
     return ()
   in
@@ -36,12 +43,48 @@ let test_push_pull channel messages =
   in
   test_lwt name test
 
+let test_send_over_recv_only =
+  let name = "send over PULL" in
+  let exn = Unix.Unix_error (Unix.EINVAL, "Nanomsg.send", "receive-only socket") in
+  let test () =
+    let socket = Nanomsg.socket Nanomsg.Pull in
+    Nanomsg.send socket "msg"
+  in
+  test_lwt_fail name exn test
+
+let test_recv_over_send_only =
+  let name = "recv from PUSH" in
+  let exn = Unix.Unix_error (Unix.EINVAL, "Nanomsg.recv", "send-only socket") in
+  let test () =
+    let socket = Nanomsg.socket Nanomsg.Push in
+    Nanomsg.recv socket
+  in
+  test_lwt_fail name exn test
+
+let test_bad_protocol =
+  let name = "connecting PUSH PAIR" in
+  let exn = Unix.Unix_error (Unix.EINVAL, "Nanomsg.recv", "send-only socket") in
+  let test () =
+    let channel = "inproc://push-pair" in
+    let push = Nanomsg.socket Nanomsg.Push in
+    let pair = Nanomsg.socket Nanomsg.Pair in
+    let _ = Nanomsg.bind push channel in
+    let _ = Nanomsg.connect pair channel in
+    Nanomsg.send push "msg"
+    >>= fun () ->
+    Nanomsg.recv pair
+  in
+  test_lwt_fail name exn test
+
 let message_samples = ["foo"; "bar"; "xoxox"]
 
 let suite = "tests">:::[
   test_push_pull "ipc:///tmp/foo" message_samples;
   test_push_pull "inproc://foo" message_samples;
   test_push_pull "tcp://127.0.0.1:5555" message_samples;
+  test_send_over_recv_only;
+  test_recv_over_send_only;
+  (* test_bad_protocol; *)
 ]
 
 let main () =
