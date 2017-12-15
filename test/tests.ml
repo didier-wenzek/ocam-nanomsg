edc359b75ceab12cba69375857f5c687f9a94f4b
Didier Wenzek <didier.wenzek@acidalie.com>
2017-12-15 11:17:35 +0100

    Port the tests to oUnit.

--- test/tests.ml
+++ test/tests.ml
@@ -1,4 +1,11 @@
 open Lwt
+open OUnit2
+
+let test_lwt name f =
+  let test test_ctxt =
+    Lwt_main.run (f ())
+  in
+  name >:: test
 
 let send_msgs socket =
   Lwt_list.iter_s (Nanomsg.send socket)
@@ -12,29 +19,34 @@ let expect_msgs socket =
     return ()
   in
   Lwt_list.iter_s expect
-    
+
+let exchange producer consumer messages = Lwt.join [
+    send_msgs producer messages;
+    expect_msgs consumer messages;
+  ]
+
+let test_push_pull channel messages =
+  let name = "push/pull over "^channel in
+  let test () =
+    let producer = Nanomsg.socket Nanomsg.Push in
+    let consumer = Nanomsg.socket Nanomsg.Pull in
+    let _ = Nanomsg.bind producer channel in
+    let _ = Nanomsg.connect consumer channel in
+    exchange producer consumer messages
+  in
+  test_lwt name test
+
+let message_samples = ["foo"; "bar"; "xoxox"]
+
+let suite = "tests">:::[
+  test_push_pull "ipc:///tmp/foo" message_samples;
+  test_push_pull "inproc://foo" message_samples;
+  test_push_pull "tcp://127.0.0.1:5555" message_samples;
+]
+
+let main () =
+  run_test_tt_main suite
+
 let () =
-  let producer = Nanomsg.socket Nanomsg.Push in
-  let consumer = Nanomsg.socket Nanomsg.Pull in
-
-(*
-  let _ = Nanomsg.bind producer "inproc://foo" in
-  let _ = Nanomsg.connect consumer "inproc://foo" in
-*)
-(*
-  let _ = Nanomsg.bind producer "ipc:///tmp/foo" in
-  let _ = Nanomsg.connect consumer "ipc:///tmp/foo" in
-*)
-(*
-  let _ = Nanomsg.connect producer "tcp://127.0.0.1:5560" in
-  let _ = Nanomsg.bind consumer "tcp://127.0.0.1:5560" in
-*)
-  let _ = Nanomsg.bind producer "ipc:///tmp/foo" in
-  let _ = Nanomsg.connect consumer "ipc:///tmp/foo" in
-
-  let msg_list = ["foo"; "bar"; "xoxox"] in
-
-  Lwt.join [
-    send_msgs producer msg_list;
-    expect_msgs consumer msg_list;
-  ] |> Lwt_main.run
+  Lwt_engine.set (new Lwt_engine.libev ());
+  Lwt_unix.with_async_detach main
